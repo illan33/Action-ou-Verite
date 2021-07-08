5bcc6747e85b38f885811e45209863b8edf94451
illan33 <75099398+illan33@users.noreply.github.com>
2021-07-08 08:51:53 +0200

    Ajoutez des fichiers projet.

---
+++ App/App/App.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+            Debug.WriteLine("OnStart");
+            if (Current.Properties.ContainsKey("PageID"))
+            {
+                var id = Current.Properties["PageID"];
+                Debug.WriteLine("OnStart - " + id);
+            }
+        }
+
+        protected override void OnSleep()
+        {
+            Debug.WriteLine("OnSleep");
+        }
+
+        protected override void OnResume()
+        {
+            Debug.WriteLine("OnResume");
+            if (Current.Properties.ContainsKey("PageID"))
+            {
+                var id = Current.Properties["PageID"];
+                Debug.WriteLine("OnResume - " + id);
+            }
+        }
+    }
+}
---
+++ App/App/JoueurPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class JoueurPage : ContentPage
+    {
+        public JoueurPage()
+        {
+            InitializeComponent();
+            Application.Current.Properties["PageID"] = "JoueurPage";
+
+            Dictionary<string,string>
+                Dico_Joueurs = new Dictionary<string,string>();
+            Console.WriteLine(Dico_Joueurs);
+        }
+
+        private void Btn_Valider_Onclick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Prénom_Completed(object sender, EventArgs e)
+        {
+            Editor prénom = (Editor)sender;
+            Console.WriteLine(prénom);
+        }
+    }
+}
